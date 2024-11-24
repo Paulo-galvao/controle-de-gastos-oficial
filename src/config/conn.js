978417bd634dddb15b72d83b73396fc067596cba
Paulo Galvão <paulogalvaoj21@gmail.com>
2024-11-24 19:52:55 -0300

    fix mongo url

--- src/config/conn.js
+++ src/config/conn.js
@@ -4,7 +4,7 @@ const mongodb = process.env.MONGODB;
 
 async function main() {
     try {
-        await mongoose.connect('mongodb+srv://paulogalvaoj21:paulo123@cluster0.uxez3.mongodb.net/controle-de-gastos');
+        await mongoose.connect(mongodb);
         console.log("Data Base connected");
         
     } catch (error) {
